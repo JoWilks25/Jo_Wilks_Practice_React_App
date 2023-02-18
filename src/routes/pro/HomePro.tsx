7d6264dd82fe757321fa72459efd24e6973d9f5d
Jo Wilks <jowilks@Jos-MacBook-Pro.local>
2023-02-18 21:22:21 +0000

    Add Radar Charts

--- src/routes/pro/HomePro.tsx
+++ src/routes/pro/HomePro.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 import styled from "styled-components"
 import useIsElementVisible from "../../utilities/useIsElementVisible"
+import RadarChart from "../../components/RadarChart";
+import { programmingData, otherSkillsData, chartOptions } from "./RadarChartData";
 
 const PageWrapper = styled.div`
-  height: 100vh;
+  height: 100%;
   width: 100%;
   margin-top: 4rem;
   display: flex;
@@ -15,6 +17,26 @@ const CenterCard = styled.div`
   height: 75%;
   padding: 2rem 5rem 0 5rem;
 `
+
+const RowWrapper = styled.div`
+  height: 85%;
+  width: 100%;
+  padding-top: 2.5rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+const ColumnWrapper = styled.div`
+  height: 85%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 0.5rem;
+`
+
 
 interface HomeProProps {
   scrollId: string;
@@ -34,12 +56,23 @@ const HomePro = ({ scrollId, setSelectedTab }: HomeProProps): React.ReactElement
     }
   }, [isVisible]);
 
+
   return (
     <PageWrapper id={scrollId} ref={scrollRef} >
       <CenterCard>
         <h1>{scrollId}</h1>
         <p>I am a senior React Developer working within the Banking sector, looking to return to Full Stack work building clean and simple products that provide value to the end user.</p>
         <p>I have a strong focus on developing good development processes and maintaining effective documentation.</p>
+        <RowWrapper>
+          <ColumnWrapper>
+            <h2>Technical Skills</h2>
+            <RadarChart data={programmingData} options={chartOptions} />
+          </ColumnWrapper>
+          <ColumnWrapper>
+            <h2>Other Skills</h2>
+            <RadarChart data={otherSkillsData} options={chartOptions} />
+          </ColumnWrapper>
+        </RowWrapper>
       </CenterCard>
     </PageWrapper>
   )
---
+++ src/routes/pro/RadarChartData.tsx
@@ -0,0 +1,50 @@
+export const programmingData: any = {
+  labels: ['Javascript', 'Typescript', 'Python', 'React (hooks based)', 'Micro Frontends', 'Git'],
+  datasets: [
+    {
+      label: 'Experience (Years)',
+      data: [4.5, 1.5, 1.5, 5, 1.5, 4.5],
+      scale: [0, 10],
+      backgroundColor: 'rgba(45, 85, 255, 0.2)',
+      borderColor: 'rgba(15, 10, 222, 1)',
+      borderWidth: 1,
+    },
+    {
+      label: 'Proficiency',
+      data: [4, 3, 3, 4, 3, 4],
+      scale: [0, 10],
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 1,
+    },
+  ],
+};
+
+export const otherSkillsData: any = {
+  labels: ['Management', 'Training/Mentoring', 'Consulting'],
+  datasets: [
+    {
+      label: 'Experience (Years)',
+      data: [2, 1.5, 3],
+      scale: [0, 10],
+      backgroundColor: 'rgba(45, 85, 255, 0.2)',
+      borderColor: 'rgba(15, 10, 222, 1)',
+      borderWidth: 1,
+    },
+    {
+      label: 'Proficiency',
+      data: [3, 3.5, 3],
+      scale: [0, 10],
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 1,
+    },
+  ],
+};
+
+export const chartOptions = {
+  scale: {
+    min: 0,
+    max: 5,
+  }
+};
