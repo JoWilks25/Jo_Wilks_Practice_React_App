7fd230b26e5452cc0f0971cbeebaf5fd8fabaa46
Jo Wilks <jowilks@Jos-MacBook-Pro.local>
2023-02-18 13:43:57 +0000

    Style cover page, add buttons link to diff main pages

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,21 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Root from './routes/Root';
+import MainPro from './routes/MainPro';
+import MainFun from './routes/MainFun';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
+    element: <Root />, // CoverPage
+  },
+  {
+    path: "/professional",
+    element: <MainPro />, // Main Page Professional Version
+  },
+  {
+    path: "/fun",
+    element: <MainFun />, // Main Page Fun Version
   },
 ]);
 
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,7 +1,65 @@
-const Root = () => {
-  return (
-    <div>Hello</div>
-  );
+import styled from "styled-components";
+
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const CenterCard = styled.div`
+  width: 50vw;
+  height: 50vh;
+`
+
+const Text = styled.div`
+  color: white;
+  font-size: 2rem;
+  font-family: sans-serif;
+`
+
+const LinkButton = styled.a`
+  text-decoration : none;
+  background-color: black;
+  color: white;
+  padding: 0.5rem 2rem;
+  font-size: 1rem;
+  font-family: sans-serif;
+  border: 2px solid white;
+  border-radius: 4px;
+  :hover {
+    background-color: white;
+    color: black;
+    border-color: grey;
+  }
+`
+
+interface RowProps {
+  justifyContent?: string;
+  padding?: string;
+  width?: string;
 }
+
+const Row = styled.div<RowProps>`
+  width: ${({ width }) => width || '100%'};
+  display: flex;
+  justify-content: ${({ justifyContent }) => justifyContent || 'center'};
+  padding: ${({ padding }) => padding || '1rem'};
+`
+
+
+const Root = (): React.ReactElement =>
+  <Background>
+    <CenterCard>
+      <Row padding="2rem 0"><Text>Welcome to Jo's Portfolio</Text></Row>
+      <Row padding="2rem 0"><Text>Please select a flavour</Text></Row>
+      <Row justifyContent="space-around">
+        <LinkButton href='/professional'>Professional</LinkButton>
+        <LinkButton href='/fun'>A Bit of Fun</LinkButton>
+      </Row>
+    </CenterCard>
+  </Background>
 
 export default Root
