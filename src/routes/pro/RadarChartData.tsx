046694eceadb951010a9091b2e5eedb87332853a
Jo Wilks <jowilks@Jos-MacBook-Pro.local>
2023-02-18 22:32:02 +0000

    Pick colours for radar chart

--- src/routes/pro/RadarChartData.tsx
+++ src/routes/pro/RadarChartData.tsx
@@ -1,3 +1,31 @@
+// PURPLE
+const purple = {
+  backgroundColor: 'rgba(151, 0, 109, 0.2)',
+  borderColor: 'rgba(151, 0, 109, 1)',
+}
+// GREEN
+const green = {
+  backgroundColor: 'rgba(0, 151, 42, 0.2)',
+  borderColor: 'rgba(0, 151, 42, 1)',
+}
+
+// YELLOW
+const yellow = {
+  backgroundColor: 'rgba(255, 215, 45, 0.2)',
+  borderColor: 'rgba(255, 215, 45, 1)',
+}
+//BLUE
+const blue = {
+  backgroundColor: 'rgba(45, 85, 255, 0.2)',
+  borderColor: 'rgba(45, 85, 255, 1)',
+}
+
+// RED
+const red = {
+  backgroundColor: 'rgba(255, 0, 0, 0.2)',
+  borderColor: 'rgba(255, 0, 0, 1)',
+}
+
 export const programmingData: any = {
   labels: ['Javascript', 'Typescript', 'Python', 'React (hooks based)', 'Micro Frontends', 'Git'],
   datasets: [
@@ -5,20 +33,19 @@ export const programmingData: any = {
       label: 'Experience (Years)',
       data: [4.5, 1.5, 1.5, 5, 1.5, 4.5],
       scale: [0, 10],
-      backgroundColor: 'rgba(45, 85, 255, 0.2)',
-      borderColor: 'rgba(15, 10, 222, 1)',
+      ...blue,
       borderWidth: 1,
     },
     {
       label: 'Proficiency',
       data: [4, 3, 3, 4, 3, 4],
       scale: [0, 10],
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
-      borderColor: 'rgba(255, 99, 132, 1)',
+      ...red,
       borderWidth: 1,
     },
   ],
 };
+
 
 export const otherSkillsData: any = {
   labels: ['Management', 'Training/Mentoring', 'Consulting'],
@@ -27,16 +54,14 @@ export const otherSkillsData: any = {
       label: 'Experience (Years)',
       data: [2, 1.5, 3],
       scale: [0, 10],
-      backgroundColor: 'rgba(45, 85, 255, 0.2)',
-      borderColor: 'rgba(15, 10, 222, 1)',
+      ...green,
       borderWidth: 1,
     },
     {
       label: 'Proficiency',
       data: [3, 3.5, 3],
       scale: [0, 10],
-      backgroundColor: 'rgba(0, 173, 21, 0.2)',
-      borderColor: 'rgba(0, 173, 21, 1)',
+      ...yellow,
       borderWidth: 1,
     },
   ],
